2684c596baa87ab953ec689124887f3211b132b6
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-08-30 02:17:43 +0900

    [Add] 더미데이터 추가

--- app/pages/CommentPage.tsx
+++ app/pages/CommentPage.tsx
@@ -1,12 +1,51 @@
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 import Comment from '../components/Comment';
+import { useState } from 'react';
+
+type CommentType = {
+    id: number;
+    profileImageUrl: string;
+    name: string;
+    comment: string;
+}
+
+const comments: CommentType[] = [
+    {
+        id: 0,
+        profileImageUrl: '',
+        name: 'sdfsdf',
+        comment: 'sdfsfd'
+    },
+    {
+        id: 1,
+        profileImageUrl: '',
+        name: 'sdfsdf',
+        comment: 'sdfsfd'
+    },
+    {
+        id: 2,
+        profileImageUrl: '',
+        name: 'sdfsdf',
+        comment: 'sdfsfd'
+    },
+]
 
 function CommentPage() {
-
+    const [comment, setComment] = useState<CommentType[]>(comments)
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Comment profileImageUrl='ddd' name='ddd' comment='ddddd'/>            
-        </View>
+                
+            <FlatList
+                    style={{ flex: 1 }}
+                    data={comment}
+                    keyExtractor={(item) => item.id.toString()}
+                    contentContainerStyle={{paddingHorizontal: 16, paddingTop: 20}}
+                    renderItem={() =>         
+                  <View style={{paddingVertical: 10}}>
+                    <Comment profileImageUrl='ddd' name='ddd' comment='ddddd'/>     
+                  </View>                                  
+                }
+                  />
+        
     );
 }
 
