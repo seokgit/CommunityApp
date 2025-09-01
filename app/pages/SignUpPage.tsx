db18776e26f2b51f62737df554903b201e456d32
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-09-01 20:52:36 +0900

    [Feat] 회원가입 기능 구현

--- app/pages/SignUpPage.tsx
+++ app/pages/SignUpPage.tsx
@@ -1,16 +1,19 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image, Button } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Button, Alert } from 'react-native';
 import TextField from '../components/TextField';
 import MainButton from '../components/MainButton';
 import { launchImageLibrary } from 'react-native-image-picker';
 import { useState } from 'react';
 // import { createUserWithEmailAndPassword } from '@react-native-firebase/auth';
 import auth, { getAuth } from '@react-native-firebase/auth';
-
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
+import { useNavigation } from '@react-navigation/native';
 
 function SignUpPage() {        
+    const navigation = useNavigation();
     const [photoUri, setPhotoUri] = useState<string | undefined>(undefined)    
-    const [email, setEmail] = useState<string>("test@test.com")
-    const [password, setPassword] = useState<string>("123456")
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
     const [nickname, setNickname] = useState<string>("")
 
     const showImagePicker = () => {          
@@ -23,13 +26,46 @@ function SignUpPage() {
     })  
 }
 
+const uploadProfileImage = async (uid: string, localUri: string): Promise<string> =>  {
+    const reference = storage().ref(`profileImages/${uid}.jpg`);
+    await reference.putFile(localUri);
+    return await reference.getDownloadURL();
+}
+
  const signUp = async () => {    
-        try {            
+        try {                        
             const user = await auth().createUserWithEmailAndPassword(email, password);
-        } catch(e) {
+            let profileImageUrl = ""
+
+            if (photoUri) {
+                profileImageUrl = await uploadProfileImage(user.user.uid, photoUri)
+            }
+
+            await firestore()
+            .collection("users")
+            .doc(user.user.uid)
+            .set({
+                email: email,
+                nickname: nickname,
+                profileImageUrl: profileImageUrl
+            })
+            
+        } catch(e: any) {
             console.log("ERROR: ",e)
+              Alert.alert(
+            "Error",
+            "네트워크 통신 중 문제가 발생했습니다.",
+      [
+        {
+          text: "아니요",
+          onPress: () => console.log("아니라는데"),
+        }             
+      ],
+      { cancelable: false }
+    );
         }
     }
+
     return (
         <View style={styles.container}>
             <TouchableOpacity onPress={showImagePicker}>
@@ -38,13 +74,13 @@ function SignUpPage() {
             </TouchableOpacity>                            
             
             <Text style={styles.label}>Email</Text>
-            <TextField/>
+            <TextField value={email} onChangeText={setEmail}/>
             <View style={{paddingVertical: 10}}/>
             <Text style={styles.label}>Password</Text>
-            <TextField/>
+            <TextField value={password} onChangeText={setPassword}/>
             <View style={{paddingVertical: 10}}/>
             <Text style={styles.label}>Nickname</Text>
-            <TextField/>
+            <TextField value={nickname} onChangeText={setNickname}/>
         <View style={{paddingVertical: 20}}/>
             <MainButton title='회원가입' onPress={signUp}/>                
         </View>
