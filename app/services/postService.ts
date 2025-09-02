f46ce43eee070766343dd0c5119c45af39428bb5
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-09-03 01:51:11 +0900

    [Fix] 게시글에 유저정보 합치기

--- app/services/postService.ts
+++ app/services/postService.ts
@@ -1,15 +1,22 @@
 import firestore from "@react-native-firebase/firestore";
 import { Post } from "../types/post";
 
-export const fetchPosts = async (): Promise<Post[]> => {
+export const fetchPosts = async (): Promise<Post[]> => {        
     const snapshot = await firestore()
     .collection("posts")
     .orderBy("createDate", "desc")
-    .get()
-
-    return snapshot.docs.map(
-        (doc) => ({id: doc.id, ...doc.data()} as Post)
+    .get()    
+   
+    const posts = await Promise.all(
+        snapshot.docs.map(async (doc) => {
+            const post = { id: doc.id, ...doc.data()} as Post;
+            const userData = (await firestore().collection("users").doc(post.userId).get()).data();            
+            
+            return {...post, profileImageUrl: userData?.profileImageUrl, authorName: userData?.nickname}
+        })
     )
+
+    return posts    
 }
 
 export const uploadPost = async (post: Post) => {    
